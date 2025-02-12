b246f7a291a8c56e369a83bb79ba6ed09a88465a
Bikash2448 <bikashcse12448@gmail.com>
2025-02-12 11:21:46 +0530

    Last commit

--- App.py
+++ App.py
@@ -24,3 +24,8 @@ def calculate():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
+
+
+# if __name__ == "__main__":
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
